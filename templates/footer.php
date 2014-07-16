46479e75b7996ed3fc8f7a897d97a6b02ac78ac9
jdcauley <jordan@cauley.co>
2014-07-16 18:43:53 -0400

    Added tech triad footer

--- templates/footer.php
+++ templates/footer.php
@@ -18,7 +18,7 @@
           <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
         </div>
         <div class="col-sm-6">
-          <a class="tech-triad" href="http://techtriad.com" title=""></a>
+          <a class="tech-triad" href="http://techtriad.com" title="Designed by TechTriad, Professional WordPress Theme Design in North Carolina" target="_blank<"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/techtriadinc.png" alt="TechTriad WordPress and Web Design Services"></a>
         </div>
       </div>
     </div>
